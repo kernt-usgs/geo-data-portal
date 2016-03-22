7de86f20e3b5f996efbf90af2c1fe4a1a22f37c6
David Uselmann <duselmann@usgs.gov>
2016-03-22 10:44:09 -0500

    remove unused (and missing) import

--- gdp-process-wps/src/test/java/gov/usgs/cida/gdp/wps/algorithm/FeatureTimeSeriesAlgorithmTests.java
+++ gdp-process-wps/src/test/java/gov/usgs/cida/gdp/wps/algorithm/FeatureTimeSeriesAlgorithmTests.java
@@ -22,7 +22,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.mock.jndi.SimpleNamingContextBuilder;
 
 import gov.usgs.cida.gdp.coreprocessing.Delimiter;
 import gov.usgs.cida.gdp.coreprocessing.analysis.timeseries.StationTimeseriesVisitor;
