cffa9326e3de8647f622f2e238a522685e90a74c
Suzanne M Larson <smlarson@usgs.gov>
2016-08-01 15:32:44 -0500

    code style and formatting updates

--- gdp-liquibase-changelogs/src/main/resources/queueThrottleTables/createTableThrottleQueueToggle.sql
+++ gdp-liquibase-changelogs/src/main/resources/queueThrottleTables/createTableThrottleQueueToggle.sql
@@ -3,7 +3,7 @@
 --changeset slarson:8createThrottleQueueToggle
 CREATE TABLE throttle_queue_toggle (
 	ID serial NOT NULL PRIMARY KEY,
-        ENABLED boolean,
+        ENABLED BOOLEAN,
         TOGGLE_TYPE VARCHAR(50))
         
 --rollback drop table throttle_queue;
--- gdp-liquibase-changelogs/src/main/resources/queueThrottleTables/insertToggleData.sql
+++ gdp-liquibase-changelogs/src/main/resources/queueThrottleTables/insertToggleData.sql
@@ -1,5 +1,5 @@
 --liquibase formatted sql
 
 --changeset slarson:1insertToggleData
-Insert into throttle_queue_toggle (enabled, toggle_type) values (false, 'THROTTLE');
-Insert into throttle_queue_toggle (enabled, toggle_type) values (false, 'REDUNDANT_REQUEST_CHECK');
+INSERT INTO throttle_queue_toggle (enabled, toggle_type) VALUES (false, 'THROTTLE');
+INSERT INTO throttle_queue_toggle (enabled, toggle_type) VALUES (false, 'REDUNDANT_REQUEST_CHECK');
