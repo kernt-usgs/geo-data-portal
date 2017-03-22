045a8d6238d4faaf9a1e7537a90e6cba7966ff45
Jordan Walker <jiwalker@usgs.gov>
2017-03-22 17:10:30 -0500

    adding analytics columns

--- gdp-liquibase-changelogs/src/main/resources/createTables/alterRequestMetadata.sql
+++ gdp-liquibase-changelogs/src/main/resources/createTables/alterRequestMetadata.sql
@@ -2,3 +2,11 @@
 
 --changeset slarson:7alterRequestMetadata
 ALTER TABLE request_metadata ALTER COLUMN user_agent TYPE TEXT;
+
+-- changeset jiwalker:8addSizeInfo
+ALTER TABLE request_metadata ADD COLUMN jobid TEXT,
+	ADD COLUMN timesteps INTEGER,
+	ADD COLUMN gridcells BIGINT,
+	ADD COLUMN varcount INTEGER,
+	ADD COLUMN cellsize_bytes SMALLINT,
+	ADD COLUMN bounding_rect TEXT;
