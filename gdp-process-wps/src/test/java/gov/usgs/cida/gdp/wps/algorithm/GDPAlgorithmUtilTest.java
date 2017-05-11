480d7695417871aa4de2627aba5a549123902a35
Jordan Walker <jiwalker@usgs.gov>
2017-05-11 13:04:27 -0500

    Addressing comments from review

--- gdp-process-wps/src/test/java/gov/usgs/cida/gdp/wps/algorithm/GDPAlgorithmUtilTest.java
+++ gdp-process-wps/src/test/java/gov/usgs/cida/gdp/wps/algorithm/GDPAlgorithmUtilTest.java
@@ -18,16 +18,16 @@ public class GDPAlgorithmUtilTest {
 	}
 
 	@Test
-	public void testDataCube_size() throws InvalidRangeException, TransformException, FactoryException {
+	public void testDataCubeSize() throws InvalidRangeException, TransformException, FactoryException {
 		// first is under 2 billion, works normally
 		GDPAlgorithmUtil.DataCube dataCubeSmall = new GDPAlgorithmUtil.DataCube(2, 3, 5, 7, null, null, null);
 		// next is over 2 billion, with just 2 dimensions
 		GDPAlgorithmUtil.DataCube dataCubeLarge = new GDPAlgorithmUtil.DataCube(1000000000, 3, 1, 1, null, null, null);
 		// next is over 2 billion only when all dimensions combined
 		GDPAlgorithmUtil.DataCube dataCubeHuge = new GDPAlgorithmUtil.DataCube(1000, 1000, 1000, 4, null, null, null);
-		assertThat(dataCubeSmall.totalSize, is(equalTo(210l)));
-		assertThat(dataCubeLarge.totalSize, is(equalTo(3000000000l)));
-		assertThat(dataCubeHuge.totalSize, is(equalTo(4000000000l)));
+		assertThat(dataCubeSmall.getTotalSize(), is(equalTo(210l)));
+		assertThat(dataCubeLarge.getTotalSize(), is(equalTo(3000000000l)));
+		assertThat(dataCubeHuge.getTotalSize(), is(equalTo(4000000000l)));
 	}
 	
 }
